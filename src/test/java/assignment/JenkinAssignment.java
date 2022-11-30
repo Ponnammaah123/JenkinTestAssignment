9d380fc8e292f7dc79f3ee18d5f66049c2dfd7b4
Ponnamma A.h <ponah@publicisgroupe.net>
2022-11-30 10:04:30 +0530

    Assignment day1 jenkins

---
+++ src/test/java/assignment/JenkinAssignment.java
@@ -0,0 +1,17 @@
+package assignment;
+
+import org.testng.annotations.Test;
+
+public class JenkinAssignment {
+
+	@Test
+	public void concatWord() {
+		
+		
+		String firstname = "Ponnamma";
+		String lastname = " A H ";
+		System.out.println("Hello"+" "+firstname.concat(lastname));
+
+	}
+
+}
